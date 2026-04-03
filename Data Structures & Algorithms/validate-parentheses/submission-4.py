8a11414dd8a63a191ee17e2ebe2185254b083e74
Anish <anish10707@gmail.com>
2026-04-02 21:01:22 -0700

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        close_stack = []
+        ref = {'{':'}','(':')','[':']'}
+        for char in s:
+            if char in ref:
+                close_stack.append(ref[char])
+            else:
+                if len(close_stack) == 0:
+                    return False
+                if not (char == close_stack[-1]):
+                    return False
+                close_stack.pop()
+        if len(close_stack)!=0:
+            return False
+        return True
+                
+
+        
