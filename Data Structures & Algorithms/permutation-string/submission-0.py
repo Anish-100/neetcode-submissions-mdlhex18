103b02633c65cbaebaa64987bbb365fe4dbed7b0
Anish <anish10707@gmail.com>
2026-06-25 13:35:36 -0700

    Update: permutation-string - submission-0

--- Data Structures & Algorithms/permutation-string/submission-0.py
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -1,29 +1,29 @@
+# Time Complexity: O(n*m), n being len(s1), m = len(s2)
+# Space Complexity: O(m)
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         exp_freq = {}
         for i in s1:
-            exp_freq[i] = 1 + exp_freq.get(i, 1)
+            exp_freq[i] = 1 + exp_freq.get(i, 0)
+        print(exp_freq)
         fixed_length = len(s1)
         l = 0
-        counter = 0
         obs_freq = {}
         if len(s2) < fixed_length:
             return False
         cur_len = 0
         for r in range(len(s2)):
-            print(s2[l:r+1])
+            counter = 0
             if cur_len >= fixed_length:
                 obs_freq[s2[l]] -=1
                 l+=1
-                obs_freq[s2[l]] = 1 + obs_freq.get(s2[l], 1)
-                if obs_freq[s2[l]] == obs_freq.get(s2[l], 0):
+            obs_freq[s2[r]] = 1 + obs_freq.get(s2[r], 0)
+            for key, val in obs_freq.items():
+                if exp_freq.get(key, -1) == val:
                     counter+=1
-            obs_freq[s2[r]] = 1 + obs_freq.get(s2[r], 1)
-            if obs_freq[s2[r]] == obs_freq.get(s2[r], 0):
-                    counter+=1
-            print(counter)
-            if sorted(s2[l:r+1]) == sorted(s1):
+            if counter == len(exp_freq):
                 return True
+        
             cur_len+=1
         return False
         
