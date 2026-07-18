53df17fe0189fcf16e6ec9ba0c7a5cff0c8db3ab
Anish <anish10707@gmail.com>
2026-07-18 11:54:57 -0700

    Add: jump-game-ii - submission-2

---
+++ Data Structures & Algorithms/jump-game-ii/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        l,r = 0,0
+        jumpsMade = 0
+        max_idx = 0
+        max_val = 0
+        while r < len(nums)-1:
+            for i in range(l,r+1):
+                print(i, nums[i])
+                if i + nums[i] > max_val:
+                    max_val = i + nums[i]
+                    max_idx = i
+            l = max_idx
+            r = max_idx+ nums[max_idx]
+            jumpsMade+=1
+            print('MAX',l,r)
+        return jumpsMade
+
+
+        
+        
