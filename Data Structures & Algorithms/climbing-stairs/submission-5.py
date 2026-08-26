0f2da52625299507d69abc64a95b8b99f96fbefd
Anish <anish10707@gmail.com>
2026-08-26 12:55:54 -0700

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        cache = {}
+        def dfs(start):
+            if start <=2:
+                cache[start] = start
+                return start
+            if start in cache:
+                return cache[start]
+            val =  dfs(start-1) + dfs(start-2)
+            cache[start] = val
+            return cache[start]
+        return dfs(n)
